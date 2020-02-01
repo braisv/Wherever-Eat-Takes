4c403c507b605e04116aeb6eb7ac531d64924f28
braisv <brais.vidal.rodriguez@gmail.com>
2020-02-01 18:18:49 +0100

    route fixed

--- server/routes/restaurants.js
+++ server/routes/restaurants.js
@@ -2,16 +2,16 @@ const express = require('express');
 const restaurantsRouter = express.Router();
 const Restaurant = require("../models/Restaurant")
 
-restaurantsRouter.post('/new', (req, res, next) => {
-  const { name, neighborhood, photograph, location, image, cuisine_type, timetable, reviews } = req.body;
-  Restaurant
-    .create({ name, neighborhood, photograph, location, image, cuisine_type, timetable, reviews })
-    .then((newRestaurant) => {
-      Restaurant
-        .findById(newRestaurant._id)
-        .then(theNewRestaurant => res.json(theNewRestaurant))
-    })
-});
+// restaurantsRouter.post('/new', (req, res, next) => {
+//   const { name, neighborhood, photograph, location, image, cuisine_type, timetable, reviews } = req.body;
+//   Restaurant
+//     .create({ name, neighborhood, photograph, location, image, cuisine_type, timetable, reviews })
+//     .then((newRestaurant) => {
+//       Restaurant
+//         .findById(newRestaurant._id)
+//         .then(theNewRestaurant => res.json(theNewRestaurant))
+//     })
+// });
 
 restaurantsRouter.get('/restaurants', (req, res, next) => {
   Restaurant
@@ -26,27 +26,20 @@ restaurantsRouter.get('/restaurant/:id', (req, res, next) => {
     .then(restaurant => res.json(restaurant))
 });
 
-// restaurantsRouter.post('/update', (req, res, next) => {
-//   const { amount, id } = req.body;
-//   Game
-//     .findByIdAndUpdate({ _id:id}, { $set: { amount:amount }}, { new: true })
-//     .then(theGame => res.json(theGame))
+// restaurantsRouter.post('/remove/:id', (req, res, next) => {
+//   const restaurantID = req.params.id
+//   Restaurant
+//     .findByIdAndRemove({ _id : restaurantID })
+//     .then(theRestaurant => res.json(theRestaurant))
 // });
 
-restaurantsRouter.post('/remove/:id', (req, res, next) => {
-  const restaurantID = req.params.id
-  Game
-    .findByIdAndRemove({ _id : restaurantID })
-    .then(theRestaurant => res.json(theRestaurant))
-});
-
-restaurantsRouter.post('/edit/:id', (req, res, next) => {
-    const { name, neighborhood, photograph, location, image, cuisine_type, timetable, reviews } = req.body;
-    const restaurantID = req.params.id
-  Restaurant
-    .findByIdAndUpdate({ _id: restaurantID }, { $set: { name, neighborhood, photograph, location, image, cuisine_type, timetable, reviews }}, { new: true })
-    .then(theRestaurant => res.json(theRestaurant))
-});
+// restaurantsRouter.post('/edit/:id', (req, res, next) => {
+//     const { name, neighborhood, photograph, location, image, cuisine_type, timetable, reviews } = req.body;
+//     const restaurantID = req.params.id
+//   Restaurant
+//     .findByIdAndUpdate({ _id: restaurantID }, { $set: { name, neighborhood, photograph, location, image, cuisine_type, timetable, reviews }}, { new: true })
+//     .then(theRestaurant => res.json(theRestaurant))
+// });
 
 
 module.exports = restaurantsRouter;
